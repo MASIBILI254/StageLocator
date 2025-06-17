038e72f25383fe572b7022f34ad00562edeea608
MASIBILI254 <andreasimiyu7@gmail.com>
2025-06-17 07:13:49 -0300

    backend url render

--- Frontend/src/services/Api.jsx
+++ Frontend/src/services/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://stagelocator-backend.onrender.com',
   withCredentials: true,
 });
 
